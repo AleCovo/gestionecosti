0561dad9729863f0cbffaf656cd7c89ceefa88f1
AleCovo <alessio64.covone@gmail.com>
2025-07-17 00:03:39 +0200

    11

--- server.js
+++ server.js
@@ -1,21 +1,55 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { text } = require('stream/consumers');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+//permette di inserire i css nelle pagine html (HTML, CSS, immagini, ecc.) dalla cartella "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+//queste 2 righe servono per utilizzare il fetch
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// Leggi il file all'avvio del server
+const filePath = path.join(__dirname, 'public/gestionecosti.txt');
+let file = '';
+let testo = [];
+
+try {
+  file = fs.readFileSync(filePath, 'utf-8');
+  console.log('Contenuto del file:', file);
+} catch (err) {
+  console.error('Errore nella lettura del file:', err);
+}
+
+testo = file.split('\n');
+const index = (testo.length + 1).toString();
+console.log('Indice per il nuovo acquisto:', index);
+
+
+
+app.listen(PORT);
+
+//lancia pagina principale
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+
+//lancia pagina per aggiungere gli acquisti
+app.get('/acquisto', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/acquisto.html'));
+});
+
+
+//lancia l'API per aggiungere gli acquisti
 app.post('/scrivi', (req, res) => {
   const { object, buyDate, sellDate, buyAmount, sellAmount } = req.body;
-  const riga = `${object}, ${buyDate}, ${sellDate}, ${buyAmount}, ${sellAmount}\n`;
-  fs.appendFile('dati.txt', riga, (err) => {
+  const riga = `\n${index},${object},${buyDate},${sellDate},${buyAmount},${sellAmount}`;
+  fs.appendFile('public/gestionecosti.txt', riga, (err) => {
     if (err) return res.status(500).send('Errore nel salvataggio');
     res.send('Dati salvati');
   });
